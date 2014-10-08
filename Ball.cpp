35c4de6de1ad95445125c5063f85e87168e17b4b
ahiijny <jiayinhuang3.14159@gmail.com>
2014-10-07 20:03:26 -0400

    Removed comment.

--- Ball.cpp
+++ Ball.cpp
@@ -6,9 +6,6 @@
 /** Initializes a Ball object of the specified dimensions.
  * Behaviour is undefined for dim < 0 and difficult for
  * humans to understand for dim > 3.
- *
- * For now, (change later), the ball's parameters are
- * initialized with random integer between 0 and 41.
  */
 Ball::Ball(int dim)
 {
